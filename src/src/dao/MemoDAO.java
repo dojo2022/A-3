4ff3a05d99d7c5392e7486d714c6d9d3327f321a
dojo2022-saito <saito-akane-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-15 14:16:53 +0900

    MemoDAO

---
+++ src/src/dao/MemoDAO.java
@@ -0,0 +1,64 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Memo;
+
+public class MemoDAO {
+
+	// insert
+	public List<Memo> insert(int page_id){
+		Connection conn = null;
+		List<Memo> memoList = new ArrayList<Memo>();//ArrayList <インスタンスの型名> 変数名 = new ArrayList<インスタンスの型名>;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+
+
+
+		}
+			catch (SQLException e) {
+				e.printStackTrace();
+				memoList = null;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				memoList = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						memoList = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return memoList;
+
+	}
+
+
+	// select
+
+
+	// update
+
+
+	//delete
+
+}
