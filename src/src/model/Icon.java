66d516d9cb48ea17ced4ec03319cf78543f80c11
dojo2022-Miyashita <103239328+dojo2022-Miyashita@users.noreply.github.com>
2022-06-15 15:13:49 +0900

    UPJoin,Icon Beans

---
+++ src/src/model/Icon.java
@@ -0,0 +1,37 @@
+package model;
+import java.io.Serializable;
+
+
+public class Icon implements Serializable{
+	private String icon_id;
+	private String icon_path;
+
+	//引数無しコンストラクタ
+	public Icon() {
+		super();
+		this.icon_id = "";
+		this.icon_path = "";
+	}
+
+
+	//ゲッターセッター
+	public String getIcon_id() {
+		return icon_id;
+	}
+
+	public void setIcon_id(String icon_id) {
+		this.icon_id = icon_id;
+	}
+
+	public String getIcon_path() {
+		return icon_path;
+	}
+
+	public void setIcon_path(String icon_path) {
+		this.icon_path = icon_path;
+	}
+
+
+
+
+}
---
+++ src/src/model/UPJoin.java
@@ -0,0 +1,53 @@
+package model;
+import java.io.Serializable;
+
+public class UPJoin implements Serializable{
+
+	private String user_page_id;
+	private String user_id;
+	private String page_id;
+
+	//引数なしコンストラクタ
+	public UPJoin() {
+		super();
+		this.user_page_id ="";
+		this.user_id ="";
+		this.page_id ="";
+	}
+
+
+	//ゲッターとセッター
+
+	public String getUser_page_id() {
+		return user_page_id;
+	}
+
+	public void setUser_page_id(String user_page_id) {
+		this.user_page_id = user_page_id;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getPage_id() {
+		return page_id;
+	}
+
+	public void setPage_id(String page_id) {
+		this.page_id = page_id;
+	}
+
+
+
+
+
+
+
+
+
+}
