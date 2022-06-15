e8fb32ad9a39d816099f865dd1abf0ae6cdadb00
dojo-teruitakayuki <103226074+dojo-teruitakayuki@users.noreply.github.com>
2022-06-15 15:24:04 +0900

    メモとカテゴリのBeans

---
+++ src/src/model/Memo.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.io.Serializable;
+
+public class Memo implements Serializable{
+private String memo_id;
+private String memo_item;
+private String memo_check;
+private String page_id;
+
+//引数なしコンストラクタ
+public Memo() {
+	this.memo_id = "";
+	this.memo_item= "";
+	this.memo_check = "";
+	this.page_id = "";
+}
+
+//setter getter
+public String getMemo_id() {
+	return memo_id;
+}
+
+
+public void setMemo_id(String memo_id) {
+	this.memo_id = memo_id;
+}
+
+
+public String getMemo_item() {
+	return memo_item;
+}
+
+
+public void setMemo_item(String memo_item) {
+	this.memo_item = memo_item;
+}
+
+
+public String getMemo_check() {
+	return memo_check;
+}
+
+
+public void setMemo_check(String memo_check) {
+	this.memo_check = memo_check;
+}
+
+
+public String getPage_id() {
+	return page_id;
+}
+
+
+public void setPage_id(String page_id) {
+	this.page_id = page_id;
+}
+
+
+}
