399fda1298b751ff242f6b85f3f0e2b52a7cfc49
dojo2022-saito <saito-akane-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-16 15:59:48 +0900

    page_flag追加したよ

--- src/src/model/Page.java
+++ src/src/model/Page.java
@@ -5,13 +5,23 @@ import java.io.Serializable;
 public class Page implements Serializable {
 
 	private String page_id; //ページID
-	private String page_title; // ページタイトル
+	private String page_title;
+	private String page_flag;// ページタイトル
 
 	//引数がないコンストラクタ
 	public Page() {
 		super();
 		this.page_id = "";
 		this.page_title = "";
+		this.page_flag = "";
+	}
+
+	public String getPage_flag() {
+		return page_flag;
+	}
+
+	public void setPage_flag(String page_flag) {
+		this.page_flag = page_flag;
 	}
 
 	public String getPage_id() {
