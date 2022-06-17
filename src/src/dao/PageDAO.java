e9dd1f35518b3d8a004b1a07e9c9d0b293a41399
dojo2022-ishiitakahiro <103226369+dojo2022-ishiitakahiro@users.noreply.github.com>
2022-06-17 11:08:09 +0900

    PageDaO

--- src/src/dao/PageDAO.java
+++ src/src/dao/PageDAO.java
@@ -2,34 +2,110 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.Page;
+import model.AllBeans;
+
 public class PageDAO {
 
-//Insert
-	public List<Page> insert(String page_title){
+	//Insert
+	public List<AllBeans> insert(AllBeans param) {
 		Connection conn = null;
-		List<Page> pageList = new ArrayList<Page>();
+		List<AllBeans> AllBeansList = new ArrayList<AllBeans>();
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			AllBeansList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			AllBeansList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
 				try {
-					// JDBCドライバを読み込む
-					Class.forName("org.h2.Driver");
-
-					// データベースに接続する
-					conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
-
-
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					AllBeansList = null;
 				}
+			}
+		}
+		// 結果を返す
+		return AllBeansList;
+	}
+
+	//select
+	public List<AllBeans> select(AllBeans param) {
+		Connection conn = null;
+		List<AllBeans> AllBeansList = new ArrayList<AllBeans>();//ArrayList <インスタンスの型名> 変数名 = new ArrayList<インスタンスの型名>;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			AllBeansList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			AllBeansList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					AllBeansList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return AllBeansList;
+	}
+
+	//deleteFlagでupdateしてデータを持ってこさせないようにする
+	public boolean deleteFlag(String page_id) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+
+				// SQL文を準備する改造する
+				String sql = "";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+
+
+				// SQL文を実行する
+						if (pStmt.executeUpdate() == 1) {
+							result = true;
+						}
+					}
 					catch (SQLException e) {
 						e.printStackTrace();
-						pageList = null;
 					}
 					catch (ClassNotFoundException e) {
 						e.printStackTrace();
-						pageList = null;
 					}
 					finally {
 						// データベースを切断
@@ -39,131 +115,11 @@ public class PageDAO {
 							}
 							catch (SQLException e) {
 								e.printStackTrace();
-								pageList = null;
 							}
-				}
+						}
+					}
+
+					// 結果を返す
+					return result;
 	}
-				// 結果を返す
-				return pageList;
-				}
-//select
-	public List<Page> select(String page_id){
-		Connection conn = null;
-		List<Page> pageList = new ArrayList<Page>();//ArrayList <インスタンスの型名> 変数名 = new ArrayList<インスタンスの型名>;
-
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
-
-
-
-		}
-			catch (SQLException e) {
-				e.printStackTrace();
-				pageList = null;
-			}
-			catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				pageList = null;
-			}
-			finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					}
-					catch (SQLException e) {
-						e.printStackTrace();
-						pageList = null;
-					}
-				}
-			}
-
-			// 結果を返す
-			return pageList;
-	}
-	//update
-	public List<Page> update(String page_id){
-		Connection conn = null;
-		List<Page> pageList = new ArrayList<Page>();//ArrayList <インスタンスの型名> 変数名 = new ArrayList<インスタンスの型名>;
-
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
-
-
-
-		}
-			catch (SQLException e) {
-				e.printStackTrace();
-				pageList = null;
-			}
-			catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				pageList = null;
-			}
-			finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					}
-					catch (SQLException e) {
-						e.printStackTrace();
-						pageList = null;
-					}
-				}
-			}
-
-			// 結果を返す
-			return pageList;
-	}
-	//delete
-	public List<Page> delete(String page_id){
-		Connection conn = null;
-		List<Page> pageList = new ArrayList<Page>();//ArrayList <インスタンスの型名> 変数名 = new ArrayList<インスタンスの型名>;
-
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-
-
-
-
-		}
-			catch (SQLException e) {
-				e.printStackTrace();
-				pageList = null;
-			}
-			catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				pageList = null;
-			}
-			finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					}
-					catch (SQLException e) {
-						e.printStackTrace();
-						pageList = null;
-					}
-				}
-			}
-
-			// 結果を返す
-			return pageList;
-	}
-	}
+}
