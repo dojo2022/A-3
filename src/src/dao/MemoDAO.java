f09c64fc5783abb3bdcf53de6c670030bd9f2955
dojo2022-ishiitakahiro <103226369+dojo2022-ishiitakahiro@users.noreply.github.com>
2022-06-17 15:30:30 +0900

    Memo DAO update

--- src/src/dao/MemoDAO.java
+++ src/src/dao/MemoDAO.java
@@ -12,7 +12,7 @@ import model.Memo;
 
 public class MemoDAO {
 	//select
-	public List<Memo> select(String page_id){
+	public List<Memo> select(String page_id) {
 		Connection conn = null;
 		List<Memo> memoList = new ArrayList<Memo>();//ArrayList <インスタンスの型名> 変数名 = new ArrayList<インスタンスの型名>;
 
@@ -27,13 +27,13 @@ public class MemoDAO {
 			String sql = "SELECT * FROM Memo WHERE page_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			pStmt.setString(1,page_id);
+			pStmt.setString(1, page_id);
 
 			// SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();//ResultSetはなんでも入る表のようなもの4
 
 			// 結果表をコレクションにコピーする
-			while (rs.next()) {		//rsインスタンスの内容を全て取り出す
+			while (rs.next()) { //rsインスタンスの内容を全て取り出す
 				Memo memo = new Memo();
 				memo.setMemo_id(rs.getString("memo_id"));
 				memo.setMemo_item(rs.getString("memo_item"));
@@ -42,35 +42,30 @@ public class MemoDAO {
 				memoList.add(memo);
 			}
 
-
-		}
-			catch (SQLException e) {
-				e.printStackTrace();
-				memoList = null;
-			}
-			catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				memoList = null;
-			}
-			finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					}
-					catch (SQLException e) {
-						e.printStackTrace();
-						memoList = null;
-					}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			memoList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			memoList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					memoList = null;
 				}
 			}
+		}
 
-			// 結果を返す
-			return memoList;
+		// 結果を返す
+		return memoList;
 	}
 
 	// update
-	public boolean update(Memo param){
+	public boolean update(Memo param) {
 		Connection conn = null;
 		boolean result = false; //結果を表す変数（初期値は失敗）
 
@@ -85,35 +80,31 @@ public class MemoDAO {
 			String sql = "update MEMO set memo_item = ?, memo_check = ? WHERE memo_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			pStmt.setString(1,param.getMemo_item());
-			pStmt.setString(2,param.getMemo_check());
-			pStmt.setString(3,param.getMemo_id());
+			pStmt.setString(1, param.getMemo_item());
+			pStmt.setString(2, param.getMemo_check());
+			pStmt.setString(3, param.getMemo_id());
 
-
-
-
-
-
-		}
-			catch (SQLException e) {
-				e.printStackTrace();
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
 			}
-			catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			finally {
-				// データベースを切断
-				if (conn != null) {
-					try {
-						conn.close();
-					}
-					catch (SQLException e) {
-						e.printStackTrace();
-					}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
 				}
 			}
+		}
 
-			// 結果を返す
-			return result;
+		// 結果を返す
+		return result;
 	}
 }
