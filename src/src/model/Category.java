26cabefd3dd155d0f07e96ef0609d98c36f22ad8
dojo2022-saito <saito-akane-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-20 15:02:39 +0900

    Category 　変更

--- src/src/model/Category.java
+++ src/src/model/Category.java
@@ -3,31 +3,35 @@ package model;
 import java.io.Serializable;
 
 public class Category implements Serializable{
-	private String category_id;
-	private String category_name;
+	private String categoryId;
+	private String categoryName;
 
 	//引数なしコンストラクタ
 	public Category() {
 
 	}
 
-
 	//setter getter
-	public String getCategory_id() {
-		return category_id;
+
+	public String getCategoryId() {
+		return categoryId;
 	}
 
-	public void setCategory_id(String category_id) {
-		this.category_id = category_id;
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
 	}
 
-	public String getCategory_name() {
-		return category_name;
+	public String getCategoryName() {
+		return categoryName;
 	}
 
-	public void setCategory_name(String category_name) {
-		this.category_name = category_name;
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
 	}
+
+
+
+
 
 
 
