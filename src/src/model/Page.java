8e52aa30b6082a03469ddaad06d210484597d92c
dojo2022-Miyashita <103239328+dojo2022-Miyashita@users.noreply.github.com>
2022-06-20 15:50:45 +0900

    PageBeans　キャメルだよ

--- src/src/model/Page.java
+++ src/src/model/Page.java
@@ -4,37 +4,43 @@ import java.io.Serializable;
 
 public class Page implements Serializable {
 
-	private String page_id; //ページID
-	private String page_title;
-	private String page_flag;// ページタイトル
+	private String pageId; //ページID
+	private String pageTitle;
+	private String pageFlag;// ページタイトル
 
 	//引数がないコンストラクタ
-	public Page() {
+	public Page(String pageId, String pageTitle, String pageFlag) {
+		super();
+		this.pageId = pageId;
+		this.pageTitle = pageTitle;
+		this.pageFlag = pageFlag;
 
 	}
 
 	//ゲッターセッター
-	public String getPage_flag() {
-		return page_flag;
+	public String getPageId() {
+		return pageId;
 	}
 
-	public void setPage_flag(String page_flag) {
-		this.page_flag = page_flag;
+	public void setPageId(String pageId) {
+		this.pageId = pageId;
 	}
 
-	public String getPage_id() {
-		return page_id;
+	public String getPageTitle() {
+		return pageTitle;
 	}
 
-	public void setPage_id(String page_id) {
-		this.page_id = page_id;
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
 	}
 
-	public String getPage_title() {
-		return page_title;
+	public String getPageFlag() {
+		return pageFlag;
 	}
 
-	public void setPage_title(String page_title) {
-		this.page_title = page_title;
+	public void setPageFlag(String pageFlag) {
+		this.pageFlag = pageFlag;
 	}
+
+
 }
