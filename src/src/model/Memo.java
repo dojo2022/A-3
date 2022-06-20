b060489936351f447d6488dbd3a4ca7d39c36bed
dojo2022-saito <saito-akane-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-20 15:49:40 +0900

    MemoBeans 変更

--- src/src/model/Memo.java
+++ src/src/model/Memo.java
@@ -3,10 +3,10 @@ package model;
 import java.io.Serializable;
 
 public class Memo implements Serializable{
-private String memo_id;
-private String memo_item;
-private String memo_check;
-private String page_id;
+private String memoId;
+private String memoItem;
+private String memoCheck;
+private String pageId;
 
 //引数なしコンストラクタ
 public Memo() {
@@ -14,44 +14,41 @@ public Memo() {
 }
 
 //setter getter
-public String getMemo_id() {
-	return memo_id;
+
+public String getMemoId() {
+	return memoId;
+}
+
+public void setMemoId(String memoId) {
+	this.memoId = memoId;
+}
+
+public String getMemoItem() {
+	return memoItem;
+}
+
+public void setMemoItem(String memoItem) {
+	this.memoItem = memoItem;
+}
+
+public String getMemoCheck() {
+	return memoCheck;
+}
+
+public void setMemoCheck(String memoCheck) {
+	this.memoCheck = memoCheck;
+}
+
+public String getPageId() {
+	return pageId;
+}
+
+public void setPageId(String pageId) {
+	this.pageId = pageId;
 }
 
 
-public void setMemo_id(String memo_id) {
-	this.memo_id = memo_id;
-}
 
-
-public String getMemo_item() {
-	return memo_item;
-}
-
-
-public void setMemo_item(String memo_item) {
-	this.memo_item = memo_item;
-}
-
-
-public String getMemo_check() {
-	return memo_check;
-}
-
-
-public void setMemo_check(String memo_check) {
-	this.memo_check = memo_check;
-}
-
-
-public String getPage_id() {
-	return page_id;
-}
-
-
-public void setPage_id(String page_id) {
-	this.page_id = page_id;
-}
 
 
 }
