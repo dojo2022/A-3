19847fba9b6351b0e92f8640b00c25d756a194ee
dojo2022-tanaka <103239306+dojo2022-tanaka@users.noreply.github.com>
2022-06-20 16:07:49 +0900

    ItemDAO　if文以外編集

--- src/src/dao/ItemDAO.java
+++ src/src/dao/ItemDAO.java
@@ -9,7 +9,8 @@ import java.sql.SQLException;
 public class ItemDAO {
 
 	//insert
-	public boolean insert(String item_name, String item_favorite , String category_id ,String page_id ,String item_alert,String stock_name ,String stock_buy ,String stock_limit ,String item_id){
+	//必須項目の時にお気に入りが登録できないからfavoriteにif文で未入力の場合はこう、とか、サーブレットで値を渡す必要があるかも
+	public boolean insert(String itemName, String itemFavorite , String categoryId ,String pageId ,String itemAlert,String stockName ,String stockBuy ,String stockLimit,String stockAlertday1 ,String stockAlertday2 ,String stockAlertday3 ,String stockAlertday4 ,String itemId ,String stockId){
 		Connection conn = null;
 		boolean result = false;
 
@@ -20,25 +21,29 @@ public class ItemDAO {
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 			// ItemテーブルのINSERT文を準備する
-			String sql1 = "INSERT INTO item (item_name , item_favorite , category_id , page_id , item_alert) VALUES ('?','?','?','?','?')";//INSERT INTO テーブル名（列名A,列名B,…） VALUES（値A,値B,…）
+			String sql1 = "INSERT INTO Item (item_name , item_favorite , category_id , page_id , item_alert) VALUES ('?','?','?','?','?')";//INSERT INTO テーブル名（列名A,列名B,…） VALUES（値A,値B,…）
 			PreparedStatement pStmt1 = conn.prepareStatement(sql1);
 
 			// SQL文を完成させる
-			pStmt1.setString(1, item_name);
-			pStmt1.setString(2, item_favorite);
-			pStmt1.setString(3, category_id);
-			pStmt1.setString(4, page_id);
-			pStmt1.setString(5, item_alert);
+			pStmt1.setString(1, itemName);
+			pStmt1.setString(2, itemFavorite);
+			pStmt1.setString(3, categoryId);
+			pStmt1.setString(4, pageId);
+			pStmt1.setString(5, itemAlert);
 
-			// StockテーブルのINSERT文を準備する
-			String sql2 = "INSERT INTO item (stock_name , stock_buy , stock_limit , item_id) VALUES ('?','?','?','?')";//INSERT INTO テーブル名（列名A,列名B,…） VALUES（値A,値B,…）
+			// StockテーブルのINSERT文を準備する（賞味期限アラートを追加する（別クラスで計算したものを持ってくる））
+			String sql2 = "INSERT INTO Stock (stock_name , stock_buy , stock_limit ,stock_alertday1 ,stock_alertday2 ,stock_alertday3 ,stock_alertday4, item_id) VALUES ('?','?','?','?')";//INSERT INTO テーブル名（列名A,列名B,…） VALUES（値A,値B,…）
 			PreparedStatement pStmt2 = conn.prepareStatement(sql2);
 
 			// SQL文を完成させる
-			pStmt1.setString(1, stock_name);
-			pStmt1.setString(2, stock_buy);
-			pStmt1.setString(3, stock_limit);
-			pStmt1.setString(4, item_id);
+			pStmt1.setString(1, stockName);
+			pStmt1.setString(2, stockBuy);
+			pStmt1.setString(3, stockLimit);
+			pStmt1.setString(4, stockAlertday1);
+			pStmt1.setString(5, stockAlertday2);
+			pStmt1.setString(6, stockAlertday3);
+			pStmt1.setString(7, stockAlertday4);
+			pStmt1.setString(8, itemId);
 
 			int ans = 0;
 			conn.setAutoCommit(false);//＝オートコミットを切る
@@ -73,8 +78,9 @@ public class ItemDAO {
 		return result;
 	}
 
-	//update
-	public boolean update(String item_name, String item_favorite , String category_id ,String page_id ,String item_alert,String stock_name ,String stock_buy ,String stock_limit ,String item_id){
+	//update、残量
+	//全部一個のメソッドにする分、残量切れ日を更新する時とそうじゃない時のif文が必要
+	public boolean update(String itemName, String itemFavorite ,String itemRemain, String itemLostday, String categoryId ,String pageId ,String itemAlert, String itemAlertday, String stockName ,String stockBuy ,String stockLimit,String stockAlert, String stockAlertday1 ,String stockAlertday2 ,String stockAlertday3 ,String stockAlertday4 ,String itemId ,String stockId){
 		Connection conn = null;
 		boolean result = false;
 
@@ -84,23 +90,33 @@ public class ItemDAO {
 
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
-			// itemテーブルのUPDATE
-			String sql1 = "UPDATE item SET item_name=? , item_favorite=? , category_id=? , page_id=? , item_alert=? where item_id=?";
+			// itemテーブルのUPDATE（買い替えアラートを更新する（別クラスで計算したものを持ってくる））
+			String sql1 = "UPDATE Item SET item_name=? , item_favorite=? ,item_remain=?, item_lostday=?, category_id=? , page_id=? , item_alert=? , item_alertday=? where item_id=?";
 			PreparedStatement pStmt1 = conn.prepareStatement(sql1);
 
-			pStmt1.setString(1, item_name);
-			pStmt1.setString(2, item_favorite);
-			pStmt1.setString(3, category_id);
-			pStmt1.setString(4, page_id);
-			pStmt1.setString(5, item_alert);
+			pStmt1.setString(1, itemName);
+			pStmt1.setString(2, itemFavorite);
+			pStmt1.setString(3, itemRemain);
+			pStmt1.setString(4, itemLostday);
+			pStmt1.setString(5, categoryId);
+			pStmt1.setString(6, pageId);
+			pStmt1.setString(7, itemAlert);
+			pStmt1.setString(8, itemAlertday);
+			pStmt1.setString(9, itemId);
 
-			String sql2 = "UPDATE stock SET stock_name=? , stock_buy=? , stock_limit=? , item_id=? where stock_id=?";
+			// stockテーブルのUPDATE（賞味期限アラートを更新する（別クラスで計算したものを持ってくる））
+			String sql2 = "UPDATE Stock SET stock_name=? , stock_buy=? , stock_limit=?, stock_alert=? ,stock_alertday1=? ,stock_alertday2=? ,stock_alertday3=? ,stock_alertday4=?, where stock_id=?";
 			PreparedStatement pStmt2 = conn.prepareStatement(sql2);
 
-			pStmt2.setString(1, stock_name);
-			pStmt2.setString(2, stock_buy);
-			pStmt2.setString(3, stock_limit);
-			pStmt2.setString(4, item_id); //item_idは絶対かわらないけどどうする
+			pStmt1.setString(1, stockName);
+			pStmt1.setString(2, stockBuy);
+			pStmt1.setString(3, stockLimit);
+			pStmt1.setString(3, stockAlert);
+			pStmt1.setString(4, stockAlertday1);
+			pStmt1.setString(5, stockAlertday2);
+			pStmt1.setString(6, stockAlertday3);
+			pStmt1.setString(7, stockAlertday4);
+			pStmt1.setString(8, stockId);
 
 			int ans=0;
 			conn.setAutoCommit(false);//＝オートコミットを切る
@@ -138,9 +154,8 @@ public class ItemDAO {
 
 	}
 
-
 	//delete
-	public boolean delete(String item_id){
+	public boolean delete(String itemId){
 		Connection conn = null;
 		boolean result = false;
 
@@ -151,18 +166,18 @@ public class ItemDAO {
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
 			// stockテーブルのDELETE
-			String sql1 = "DELETE FROM stock WHERE stock_id = '?';";
+			String sql1 = "DELETE FROM Stock WHERE item_id = '?'";
 			PreparedStatement pStmt1 = conn.prepareStatement(sql1);
 
 			// SQL文を完成させる
-			pStmt1.setString(1, item_id);
+			pStmt1.setString(1, itemId);
 
 			// ItemテーブルのDELETE
-			String sql2 = "DELETE FROM stock WHERE item_id = '?';";
+			String sql2 = "DELETE FROM Item WHERE item_id = '?'";
 			PreparedStatement pStmt2 = conn.prepareStatement(sql2);
 
 			// SQL文を完成させる
-			pStmt2.setString(1, item_id);
+			pStmt2.setString(1, itemId);
 
 			int ans = 0;
 			conn.setAutoCommit(false);//＝オートコミットを切る
