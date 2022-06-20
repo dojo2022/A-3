1ffb24d7145a8b39638d07e871f43873ac330ed7
dojo2022-Miyashita <103239328+dojo2022-Miyashita@users.noreply.github.com>
2022-06-20 14:48:16 +0900

    Memoキャメル

--- src/src/dao/MemoDAO.java
+++ src/src/dao/MemoDAO.java
@@ -12,7 +12,7 @@ import model.Memo;
 
 public class MemoDAO {
 	//select
-	public List<Memo> select(String page_id) {
+	public List<Memo> select(String pageId) {
 		Connection conn = null;
 		List<Memo> memoList = new ArrayList<Memo>();//ArrayList <インスタンスの型名> 変数名 = new ArrayList<インスタンスの型名>;
 
@@ -27,7 +27,7 @@ public class MemoDAO {
 			String sql = "SELECT * FROM memo WHERE page_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			pStmt.setString(1, page_id);
+			pStmt.setString(1, pageId);
 
 			// SQL文を実行し、結果表を取得する
 			ResultSet rs = pStmt.executeQuery();//ResultSetはなんでも入る表のようなもの4
@@ -35,9 +35,9 @@ public class MemoDAO {
 			// 結果表をコレクションにコピーする
 			while (rs.next()) { //rsインスタンスの内容を全て取り出す
 				Memo memo = new Memo();
-				memo.setMemo_id(rs.getString("memo_id"));
-				memo.setMemo_item(rs.getString("memo_item"));
-				memo.setMemo_check(rs.getString("memo_check"));
+				memo.setMemoId(rs.getString("memoId"));
+				memo.setMemoItem(rs.getString("memoItem"));
+				memo.setMemoCheck(rs.getString("memoCheck"));
 
 				memoList.add(memo);
 			}
@@ -65,7 +65,7 @@ public class MemoDAO {
 	}
 
 	// update
-	public boolean update(String memo_item, String memo_check, int memo_id) {
+	public boolean update(String memoItem, String memoCheck, String memoId) {
 		Connection conn = null;
 		boolean result = false; //結果を表す変数（初期値は失敗）
 
@@ -80,9 +80,9 @@ public class MemoDAO {
 			String sql = "update MEMO set memo_item = ?, memo_check = ? WHERE memo_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
-			pStmt.setString(1,memo_item);
-			pStmt.setString(2,memo_check);
-			pStmt.setInt(3,memo_id);
+			pStmt.setString(1,memoItem);
+			pStmt.setString(2,memoCheck);
+			pStmt.setString(3,memoId);
 
 			// SQL文を実行する
 			if (pStmt.executeUpdate() == 1) {
