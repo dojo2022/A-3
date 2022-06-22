b13ea6f0451f90bc0762e2d28f1dd0d415742870
dojo2022-tanaka <103239306+dojo2022-tanaka@users.noreply.github.com>
2022-06-23 08:32:34 +0900

    tablist.js 画像が変更できるようになったよ

--- src/WebContent/javaScript/tab_list.js
+++ src/WebContent/javaScript/tab_list.js
@@ -1,5 +1,5 @@
 //お気に入りボタンの変更
-var img_src = new Array("/syokuzaikanri/img/alert_on.png","/syokuzaikanri/img/alert_off.png");
+var favorite_img_src = new Array("/syokuzaikanri/img/favorite_off.png","/syokuzaikanri/img/favorite_on.png");
 var i = 0;
 function favoriteImage(){
     if (i == 1) {
@@ -7,33 +7,33 @@ function favoriteImage(){
     } else {
       i ++;
     }
-    document.getElementById("favoriteImage").src = img_src[i];
+    document.getElementById("favoriteImage").src = favorite_img_src[i];
 }
 //お気に入りのAjax
 
 
 //賞味期限アラートのボタンの変更
-var img_src = new Array("/syokuzaikanri/img/alert_on.png","/syokuzaikanri/img/alert_off.png");
-var i = 0;
+var alert_img_src = new Array("/syokuzaikanri/img/alert_on.png","/syokuzaikanri/img/alert_off.png");
+var j = 0;
 function alertlImage(){
-    if (i == 1) {
-      i = 0;
+    if (j == 1) {
+      j = 0;
     } else {
-      i ++;
+      j ++;
     }
-    document.getElementById("alertlImage").src = img_src[i];
+    document.getElementById("alertlImage").src = alert_img_src[j];
 }
 //賞味期限アラートのAjax
 
 
 //在庫登録アコーディオンのボタンの変更
-var img_src = new Array("/syokuzaikanri/img/stockadd_plus.png","/syokuzaikanri/img/stockadd_minus.png");
-var i = 0;
+var stockadd_img_src = new Array("/syokuzaikanri/img/stockadd_plus.png","/syokuzaikanri/img/stockadd_minus.png");
+var k = 0;
 function stockAddButton(){
-    if (i == 1) {
-      i = 0;
+    if (k == 1) {
+      k = 0;
     } else {
-      i ++;
+      k ++;
     }
-    document.getElementById("stockAddButton").src = img_src[i];
+    document.getElementById("stockAddButton").src = stockadd_img_src[k];
 }
