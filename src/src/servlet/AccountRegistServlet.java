8e637ccaa0604b974e65a8f8428f0be069fbfd80
dojo2022-kimura <103239383+dojo2022-kimura@users.noreply.github.com>
2022-06-24 02:58:01 +0900

    登録後にメインページに行けるよ

--- src/src/servlet/AccountRegistServlet.java
+++ src/src/servlet/AccountRegistServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.UserDAO;
+import model.User;
 
 /**
  * Servlet implementation class AccountRegistServlet
@@ -29,15 +30,16 @@ public class AccountRegistServlet extends HttpServlet {
 		//リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		String userName = request.getParameter("userName");
-		String user = request.getParameter("userId");
+		String userId = request.getParameter("userId");
 		String userPw = request.getParameter("userPw");
 
 		UserDAO uDao = new UserDAO();
-		if (uDao.insert(user,userPw,userName)) {	// 登録成功
-			// セッションスコープにIDを格納する
+		if (uDao.insert(userId,userPw,userName)) {	// 登録成功
+			//ログイン処理をする
+			User user = uDao.Login(userId, userPw);
+			// セッションスコープにUserBeansを格納する
 			HttpSession session = request.getSession();
-
-			session.setAttribute("userId",user);
+			session.setAttribute("user",user);
 			// ウェルカムサーブレットにリダイレクトする
 			response.sendRedirect("/syokuzaikanri/WelcomeServlet");
 		}
